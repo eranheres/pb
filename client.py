8de1d4c95d48032e3b649861daf770215cf9d876
Eran Heres <eran.heres@gmail.com>
2015-06-18 23:55:18 -0700

    adding config support

--- client.py
+++ client.py
@@ -1,15 +1,18 @@
+import sys
 import socket
 import sys
 from PIL import ImageGrab
 import win32gui
 import struct
+import json
+from pprint import pprint
 
-PORT = 9999
-PATTERN = 'command'
 
-
-def capture_image():
+def capture_image(config):
     toplist, winlist = [], []
+    pattern = config["client"]["pattern"]
+    host = config['client']['host']
+    port = config['client']['port']
 
     def enum_cb(hwnd, results):
         winlist.append((hwnd, win32gui.GetWindowText(hwnd)))
@@ -18,7 +21,7 @@ def capture_image():
     for win in winlist:
         title = win[1]
         hwnd = win[0]
-        if PATTERN.lower() not in title.lower():
+        if pattern.lower() not in title.lower():
             continue
         # just grab the hwnd for first window matching firefox
         win32gui.SetForegroundWindow(hwnd)
@@ -26,15 +29,15 @@ def capture_image():
         img = ImageGrab.grab(bbox)
         filename = title + '.tmp.bmp'
         img.save(filename, "BMP")
-        send_image(title)
+        send_image(title, host, port)
 
 
-def send_image(title):
+def send_image(title, host, port):
     # Create a TCP/IP socket
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
     # Connect the socket to the port where the server is listening
-    server_address = ('localhost', 9999)
+    server_address = (host, port)
     print >>sys.stderr, 'connecting to %s port %s' % server_address
     sock.connect(server_address)
 
@@ -50,5 +53,23 @@ def send_image(title):
     sock.shutdown(socket.SHUT_RDWR)
     sock.close()
 
-capture_image()
-print 'done'
+
+def read_config(filename):
+    with open(filename) as data_file:
+        data = json.load(data_file)
+    return data
+
+
+def main():
+    config_filename = 'config.json'
+    if len(sys.argv) >= 2:
+        config_filename = sys.argv[1]
+    print 'configuration received from '+config_filename+':'
+    config = read_config(config_filename)
+    pprint(config)
+    capture_image(config)
+    print 'done'
+
+
+if __name__ == "__main__":
+        main()
